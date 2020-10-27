467bb254cb294674192d73f5352bfde1f53003e5
aoiyamada123 <57698590+aoiyamada123@users.noreply.github.com>
2020-10-27 16:00:41 +0800

    tp

---
+++ 2DGame/Assets/場景/mainpagemanger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Mainpagemanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+  void gamestard()
+        {
+
+        }
+  void endgame()
+        {
+
+        }
+    }
+}
