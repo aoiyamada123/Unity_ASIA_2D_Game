465ee85e49fb1a29ad7073ad902becc8dd406bd6
aoiyamada123 <57698590+aoiyamada123@users.noreply.github.com>
2020-11-03 16:02:49 +0800

    end game game satrt

---
+++ 2DGame/Assets/Sunnyland/artwork/Sprites/mainpagemanger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainpagemanger : MonoBehaviour
+{
+    
+    public void gamestard()
+        {
+        print("new game");
+        SceneManager.LoadScene("關卡1");
+
+        }
+  public void endgame()
+        {
+        print("end game");
+        Application.Quit();
+        }
+
+}
---
+++ 2DGame/Assets/Sunnyland/artwork/Sprites/player.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class player: MonoBehaviour
+{
+    public GameObject final;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("tough the tp");
+    }
+
+}
